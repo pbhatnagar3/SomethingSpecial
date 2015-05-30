10adda4fce482f21e44f261043a089978241b64f
Pujun Bhatnagar <pbhatnagar3@gatech.edu>
2015-05-30 15:27:32 -0700

    implement base layout

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,10 +1,21 @@
 from flask import Flask
+from flask import render_template
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
 	return 'Hello world'
 
+@app.route('/entries')
+def entries():
+	file_name = 'templates/DailyEntries/May27'
+	with open(file_name) as f:
+		content = f.readlines()
+	return render_template('entries.html', entry=content)
+	# return 'entries'
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
+
