64efb0fa365cb527094cf6b3ab491d4fb6896ae8
Pujun Bhatnagar <pbhatnagar3@gatech.edu>
2015-06-01 00:00:30 -0700

    add HIGHLIGHT and Specific Title

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask import render_template
 import os
+import json
+from pprint import pprint
+
 
 app = Flask(__name__)
 
@@ -10,12 +13,23 @@ def hello_world():
 
 @app.route('/entries')
 def entries(entry='May27'):
-	entry_loc = 'static/DailyEntries/' + entry
+	entry_loc = 'static/DailyEntries/' + entry + '.json'
 	image = '/static/images/' + entry + '.jpg'
 	print "image URL", image
 	with open(entry_loc) as f:
 		content = f.readlines()
-	return render_template('entries.html', entry = content, image = image)
+	with open(entry_loc) as data_file:    
+	    json_content = json.load(data_file)
+	print json_content
+	specific_title = "Specific Title"
+	return render_template('entries.html', 
+		date=json_content['date'],
+	 	title = json_content['title'],
+	 	highlight = json_content['highlight'],
+	 	image = image,
+	 	specific_title = specific_title,
+	 	specific_content = json_content['family']
+	 	)
 
 if __name__ == '__main__':
 	app.run(debug=True)
